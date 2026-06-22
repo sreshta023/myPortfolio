fd06a7f91d109ac4fe3d5e39890e57457ea306a1
Sreshta <sreshtapasunooru@gmail.com>
2026-06-22 18:27:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Message Sent Successfully!");
+
+    form.reset();
+
+});
